6483ddcca5d6306694e3a23a0e9acfa27268c0c8
MediocreWizard <>
2025-03-14 21:16:23 +0000

    Split long line

--- tamu/t-minus/scripts/list_suid_sgid.sh
+++ tamu/t-minus/scripts/list_suid_sgid.sh
@@ -11,7 +11,24 @@ sudo sh -c '
 COLOR_RED=\"\\033[0;91m\"
 COLOR_NONE=\"\\033[0m\"
 
-gtfobins_suid=\"aa-exec ab agetty alpine ar arj arp as ascii-xfr ash aspell atobm awk base32 base64 basenc basez bash bc bridge busctl busybox bzip2 cabal capsh cat chmod choom chown chroot clamscan cmp column comm cp cpio cpulimit csh csplit csvtool cupsfilter curl cut dash date dd debugfs dialog diff dig distcc dmsetup docker dosbox ed efax elvish emacs env eqn espeak expand expect file find fish flock fmt fold gawk gcore gdb genie genisoimage gimp grep gtester gzip hd head hexdump highlight hping3 iconv install ionice ip ispell jjs join jq jrunscript julia ksh ksshell kubectl ld.so less links logsave look lua make mawk minicom more mosquitto msgattrib msgcat msgconv msgfilter msgmerge msguniq multitime mv nasm nawk ncftp nft nice nl nm nmap node nohup ntpdate od openssl openvpn pandoc paste perf perl pexec pg php pidstat pr ptx python rc readelf restic rev rlwrap rsync rtorrent run-parts rview rvim sash scanmem sed setarch setfacl setlock shuf soelim softlimit sort sqlite3 ss ssh-agent ssh-keygen ssh-keyscan sshpass start-stop-daemon stdbuf strace strings sysctl systemctl tac tail taskset tbl tclsh tee terraform tftp tic time timeout troff ul unexpand uniq unshare unsquashfs unzip update-alternatives uudecode uuencode vagrant varnishncsa view vigr vim vimdiff vipw w3m watch wc wget whiptail xargs xdotool xmodmap xmore xxd xz yash zsh zsoelim\"
+gtfobins_suid=\"aa-exec ab agetty alpine ar arj arp as ascii-xfr ash aspell atobm
+awk base32 base64 basenc basez bash bc bridge busctl busybox bzip2 cabal capsh
+cat chmod choom chown chroot clamscan cmp column comm cp cpio cpulimit csh csplit
+csvtool cupsfilter curl cut dash date dd debugfs dialog diff dig distcc dmsetup
+docker dosbox ed efax elvish emacs env eqn espeak expandexpect file find fish
+flock fmt fold gawk gcore gdb genie genisoimage gimp grep gtester gzip hd head
+hexdump highlight hping3 iconv install ionice ip ispell jjs join jq jrunscript
+julia ksh ksshell kubectl ld.so less links logsave look lua make mawk minicom more
+mosquitto msgattrib msgcat msgconv msgfilter msgmerge msguniq multitime mv nasm
+nawk ncftp nft nice nl nm nmap node nohup ntpdate od openssl openvpn pandoc paste
+perf perl pexec pg php pidstat pr ptx python rc readelf restic rev rlwrap rsync
+rtorrent run-parts rview rvim sash scanmem sed setarch setfacl setlock shuf soelim
+softlimit sort sqlite3 ss ssh-agent ssh-keygen ssh-keyscan sshpass
+start-stop-daemon stdbuf strace strings sysctl systemctl tac tail taskset tbl
+tclsh tee terraform tftp tic time timeout troff ul unexpand uniq unshare
+unsquashfs unzip update-alternatives uudecode uuencode vagrant varnishncsa view
+vigr vim vimdiff vipw w3m watch wc wget whiptail xargs xdotool xmodmap xmore xxd
+xz yash zsh zsoelim\"
 
 files_suid_sgid=\$(find / -type f -perm -2000 -o -perm -4000  2>/dev/null)
 
